e79a6286d5823849d593a42bf81cff6636a20728
Kalarrs Topham <k-dawg-klark@hotmail.com>
2018-11-16 00:27:22 -0700

    Project commands (#7)
    * KAL-28 use destructuring.
    
    * KAL-21 switch to enquirer. Now can do multiple choice etc.
    
    * KAL-83 fix bug with git init. Updated check to check if directory is under any git control.
    
    * KAL-21 save point
    
    * KAL-21 added notes
    
    * KAL-21 Save point. Added project utils. Added ability to execut yarn (install) in a specific directory. Added capture of project details.
    
    * KAL-83 tested and it works! Nice.
    
    * KAL-83 Added alias for create/new. Added install of serverless to workspace. Added project init, for existing project.
    
    * KAL-83 Adding hacky clone of libs. This should be a typescript repo and installed with yarn, but you know.
    KAL-28 missed fs imports.
    
    * KAL-83 Fixed bug. Added validation for workspace username. Made messages more clear.
    
    * KAL-83 Ugh, ghetto fix for npm in libs. It should be it's own npm package.
    
    * KAL-83 remove dep for another change set
    
    * bump

--- bin/kalarrs.js
+++ bin/kalarrs.js
@@ -8,6 +8,7 @@ const programUtil = require('../lib/util/programs-util');
 const gitUtil = require('../lib/util/git-util');
 const yarnUtil = require('../lib/util/yarn-util');
 const serverlessUtil = require('../lib/util/serverless-util');
+const projectUtil = require('../lib/util/project-util');
 
 program
     .version(version)
@@ -51,7 +52,7 @@ program
 
                 const hasYarn = await yarnUtil.checkForInit(workspacePath);
                 if (hasYarn) await yarnUtil.checkForWorkspaceDependencies(workspacePath);
-                if (hasYarn) await yarnUtil.installPackages();
+                if (hasYarn) await yarnUtil.installPackages(workspacePath);
 
                 await serverlessUtil.checkForWorkspaceUserYaml(workspacePath);
                 await serverlessUtil.checkForWorkspaceDevEnvYaml(workspacePath);
@@ -61,5 +62,76 @@ program
                 throw new Error(`Unrecognized workspace command ${cmd}`);
         }
     });
+
+program
+    .command('project <subCommand>')
+    .option('-p, --path <path>', 'path to workspace')
+    .description('create a @kalarrs serverless project from a template')
+    .action(async (subCommand, cmd) => {
+        // WOOT
+        // https://stackoverflow.com/questions/42480949/what-do-the-curly-braces-do-in-switch-statement-after-case-in-es6
+        switch (subCommand.toLowerCase()) {
+            case 'new':
+            case 'create': {
+                /*
+                    Configure .idea for ts
+                    Configure .idea for project folders :)
+                */
+
+                const workspacePath = cmd.path ? path.join(process.cwd(), cmd.path) : process.cwd();
+                const projectName = await projectUtil.projectName();
+                const projectPath = path.join(workspacePath, projectName);
+                const projectLanguage = await projectUtil.projectLanguage();
+                const template = await projectUtil.template(projectLanguage);
+                const templateUrl = `https://github.com/kalarrs/serverless-template-${projectLanguage}/tree/master/aws/${template}`;
+                await serverlessUtil.create(templateUrl, projectName);
+
+                const hasYarn = await yarnUtil.checkForInit(projectPath);
+                if (hasYarn) await yarnUtil.installPackages(projectPath);
+
+                if (projectLanguage === 'typescript') {
+                    const hasYarn = await yarnUtil.checkForInit(workspacePath);
+                    if (hasYarn) await yarnUtil.checkForWorkspaceTypeScriptDependencies(workspacePath);
+                    await gitUtil.cloneWorkspaceTypescriptLibs(workspacePath);
+                    const lambdaUtil = path.join(workspacePath,'libs/lambda-util');
+                    await yarnUtil.installPackages(lambdaUtil);
+                    const mongooseUtil = path.join(workspacePath,'libs/mongoose-util');
+                    await yarnUtil.installPackages(mongooseUtil);
+                }
+
+                //await webstormUtil.autoCompileTypeScript(); // TODO : Add .idea/misc.xml which sets TypeScript to autocompile
+                //await webstormUtil.configureProjectSourceFolders(); // TODO: Configure .idea/ to set sourceRoot on project dir and project/src dir
+
+                break;
+            }
+            case 'init': {
+                const projectPath = cmd.path ? path.join(process.cwd(), cmd.path) : process.cwd();
+                const workspacePath = path.join(projectPath, '../');
+                // TODO : Autodetect and fallback to asking if not detected.
+                const projectLanguage = await projectUtil.projectLanguage();
+
+                const hasYarn = await yarnUtil.checkForInit(projectPath);
+                if (hasYarn) await yarnUtil.installPackages(projectPath);
+
+                if (projectLanguage === 'typescript') {
+                    const hasYarn = await yarnUtil.checkForInit(workspacePath);
+                    if (hasYarn) await yarnUtil.checkForWorkspaceTypeScriptDependencies(workspacePath);
+                    await gitUtil.cloneWorkspaceTypescriptLibs(workspacePath);
+                    const lambdaUtil = path.join(workspacePath,'libs/lambda-util');
+                    await yarnUtil.installPackages(lambdaUtil);
+                    const mongooseUtil = path.join(workspacePath,'libs/mongoose-util');
+                    await yarnUtil.installPackages(mongooseUtil);
+                }
+
+                //await webstormUtil.autoCompileTypeScript(); // TODO : Add .idea/misc.xml which sets TypeScript to autocompile
+                //await webstormUtil.configureProjectSourceFolders(); // TODO: Configure .idea/ to set sourceRoot on project dir and project/src dir
+
+                break;
+            }
+            default:
+                throw new Error(`Unrecognized project command ${cmd}`);
+        }
+    });
+// --language=c# --name=foo
 
 program.parse(process.argv);
